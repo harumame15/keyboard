028058b6c8d5cfb6e25cccf57f7c9662d290f008
harumame <kumakyoro15@yahoo.co.jp>
2024-01-08 20:26:43 +0900

    vial

---
+++ harumame1st67/qmk-firmware/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA7, 0xDA, 0xCA, 0xCB, 0x90, 0x3A, 0x5D, 0xCA}
+/* Vial unlock keycombo: ESC + Return */
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
+
+#define ENCODERS_PAD_A { GP27 , GP15 }
+#define ENCODERS_PAD_B { GP26 , GP14 }
+#define ENCODER_RESOLUTION 4
